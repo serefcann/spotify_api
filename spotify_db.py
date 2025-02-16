768e1ee574adb5e15933edf3dac6d57a25a6f1d0
şeref can memiş <canmemis@marun.edu.tr>
2025-02-17 02:04:38 +0300

    database system improvement

---
+++ spotify_db.py
@@ -0,0 +1,89 @@
+from dotenv import load_dotenv
+import pandas as pd
+import mysql.connector
+import datetime
+from track import recently_played_track
+import os
+
+class spotify_db:
+    def __init__(self):
+        load_dotenv("C:\\Users\\şerefcanmemiş\\Documents\\Projects\\spoti\\.env")
+        self.conn = self.connect_db()
+        self.mycursor = self.conn.cursor()
+        self.token = os.getenv("TOKEN")
+
+    def connect_db(self):
+        mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=os.getenv("PASSWORD"),
+        database="spotify")
+        return mydb
+
+    def create_table(self):
+        self.mycursor.execute("CREATE TABLE IF NOT EXISTS \
+            spotify_track (name VARCHAR(255), \
+            artist VARCHAR(255),\
+            artist_id VARCHAR(255), \
+            popularity INT,\
+            release_date DATE\
+            saved_at DATETIME)")
+
+    def get_past_batch(self):
+        self.mycursor.execute("SELECT name FROM spotify_track\
+                        ORDER BY saved_at DESC LIMIT 40")
+        rows = self.mycursor.fetchall()
+        return rows
+    
+    def get_nonoverlapping_tracks(self,new_batch):
+        past_batch = self.get_past_batch()
+        nonmatch_list = []
+        index = 0
+        same_track = 0
+        i=0
+        while index < len(new_batch) and i < len(past_batch):
+            if past_batch[i][0] != new_batch[index][0]: # 0 3 1 4 
+                nonmatch_list.append(new_batch[index])
+                print(past_batch[i][0],new_batch[index])
+                index += 1         
+            else:
+                index += 1
+                same_track +=1
+                i +=1
+            if same_track >=10:
+                break
+            if index >= len(new_batch):
+                i += 1
+                index = 0  # new_batch_list baştan kontrol edilmeli
+        return nonmatch_list
+    
+    def store_tracks(self,tracks_to_store):
+        insertion = """INSERT INTO spotify_track (name, artist, artist_id, popularity, release_date, saved_at) 
+                VALUES (%s, %s, %s, %s, %s, %s)"""
+        for track in tracks_to_store:
+            print(track[0])
+        answer = input("Bu sarkilari database'de depolamak istiyorsan 'yes' istemiyorsan 'no' yaz: ")
+        
+        if answer == 'yes':
+            self.mycursor.executemany(insertion,tracks_to_store)
+            self.conn.commit()
+            print("depolandi")
+            self.conn.close()
+            self.mycursor.close()       
+        else:
+            print("sarkilar depolanmadi")
+            self.conn.close()
+            self.mycursor.close()
+        
+
+
+if __name__  == "__main__":
+    spotify=spotify_db()
+    recent_track_list = recently_played_track(token=os.getenv("TOKEN"),limit=50)
+    tracks_to_store = spotify.get_nonoverlapping_tracks(new_batch=recent_track_list)
+    spotify.store_tracks(tracks_to_store=tracks_to_store)
+
+
+    
+
+
---
+++ track.py
@@ -0,0 +1,70 @@
+import os
+from dotenv import load_dotenv
+import dotenv
+import requests
+import pandas as pd
+import datetime
+
+
+load_dotenv("C:\\Users\\şerefcanmemiş\\Documents\\Projects\\spoti\\.env")
+token = os.getenv("TOKEN")
+
+def recently_played_track(token,limit):
+    url = "https://api.spotify.com/v1/me/player/recently-played"
+    header = {
+        "Authorization":f"Bearer {token}",
+    }
+    params = {
+        "market":"TR",
+        "limit":limit
+    }
+    response = requests.get(url=url,headers=header,params=params)
+    
+    if response.status_code !=200:
+        print(response.text)
+    last_tracks = response.json().get("items")
+    values_list = []
+    for track in last_tracks:
+        track_name = track.get("track").get("name")
+        track_artist = track.get("track").get("album").get("artists")[0].get("name")
+        track_artist_id = track.get("track").get("album").get("artists")[0].get("id")
+        track_popularity = track.get("track").get("popularity",0)
+        track_release = track.get("track").get("album").get("release_date")
+        values_list.append((track_name,track_artist,track_artist_id,track_popularity,track_release,datetime.datetime.now()))
+        
+    return values_list
+
+
+
+def artist_togenres(token,artist_id):
+    artist_url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    header = {
+        "Authorization":f"Bearer {token}"
+    }
+    response = requests.get(artist_url,headers=header)
+    # name = response.json().get("name")
+    # print(f"current artist name: {name}")
+    return response.json().get("genres")
+#artist_togenres(token=token,artist_id=last_track_artist_id)
+
+def search_track(token,genres,limit):
+    url = "https://api.spotify.com/v1/search"
+    header = {
+        "Authorization":f"Bearer {token}"
+    }
+    all_tracks = [] 
+    for genre in genres:
+        params = {
+            "q":f"genre:{genre}",
+            "type":"track",
+            "market":"TR",
+            "limit":limit
+        }
+        response = requests.get(url=url,headers=header,params=params)
+        if response.status_code !=200:
+            print(response.text)
+            continue
+        results = response.json().get("tracks", {}).get("items", [])
+        all_tracks.extend(results)  # Merge all results
+    return all_tracks
+#search_track(token=token,genres=['turkish hip hop', 'turkish pop'],limit=5)
